efe635e5d4aabb990501075190aa3d47bc6b844a
Eduardo <lalotahoma@icloud.com>
2018-02-11 19:04:32 -0600

    Remove merge notation from sample.module.ts

--- src/app/main/content/sample/sample.module.ts
+++ src/app/main/content/sample/sample.module.ts
@@ -5,11 +5,10 @@ import { SharedModule } from '../../../core/modules/shared.module';
 
 import { FuseSampleComponent } from './sample.component';
 import { CreateGameComponentComponent } from './create-game-component/create-game-component.component';
-<<<<<<< HEAD
+
+
 import { FuseWidgetModule } from '../../../core/components/widget/widget.module';
-=======
 import { ViewGameComponentComponent } from './view-game-component/view-game-component.component';
->>>>>>> 4880283aa4812e6e2265987257e41d4374c2eb32
 
 const routes = [
     {
