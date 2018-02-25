4f9b45a39ab4e0281e9038515a03d35a20574d08
Andrea Osorio <a.wewe95@gmail.com>
2018-02-24 20:59:15 -0600

    Added i18n files

---
+++ src/app/main/content/login/i18n/en.ts
@@ -0,0 +1,15 @@
+export const locale = {
+  lang: 'en',
+  data: {
+    'LOGININFO' : {
+      'FULLNAME' : 'Full name',
+      'EMAIL' : 'Email',
+      'PASSWORD' : 'Password',
+      'PASS_CONF' : 'Repeat Password',
+      'CREATE_BUTTON' : 'CREATE ACCOUNT',
+      'OR' : 'or',
+      'FACEBOOK' : 'Sign in with Facebook',
+      'GOOGLE' : 'Sign in with Google',
+    }
+  }
+};
---
+++ src/app/main/content/login/i18n/es.ts
@@ -0,0 +1,15 @@
+export const locale = {
+  lang: 'es',
+  data: {
+    'LOGININFO' : {
+      'FULLNAME' : 'Nombre completo',
+      'EMAIL' : 'Correo electrónico',
+      'PASSWORD' : 'Contraseña',
+      'PASS_CONF' : 'Repetir Contraseña',
+      'CREATE_BUTTON' : 'CREAR CUENTA',
+      'OR' : 'ó',
+      'FACEBOOK' : 'Inicia sesión con Facebook',
+      'GOOGLE' : 'Inicia sesión con Google',
+    }
+  }
+};
