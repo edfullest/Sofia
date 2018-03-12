e9114b2b656e064c10eaaa199fa510720ed7f02a
Andrea Osorio <a.wewe95@gmail.com>
2018-03-11 22:28:28 -0600

    Added FormControl and Score of Read of games

---
+++ src/app/main/content/games/view-game-component/i18n/en.ts
@@ -0,0 +1,14 @@
+export const locale = {
+  lang: 'en',
+  data: {
+      'GAME' : {
+          'PREVIOUS' : 'Previous',
+          'NEXT' : 'Next',
+          'SUBMITBUTTON' : 'Submit',
+          'CLEARBUTTON' : 'Clear',
+          'PLACEHOLDER_ANS' : 'Type answer here',
+          'END_MSG' : 'You did it! ',
+          'SCORE_MSG' : 'You got this score: '
+      }
+  }
+};
---
+++ src/app/main/content/games/view-game-component/i18n/es.ts
@@ -0,0 +1,14 @@
+export const locale = {
+  lang: 'es',
+  data: {
+      'GAME' : {
+          'PREVIOUS' : 'Anterior',
+          'NEXT' : 'Siguiente',
+          'SUBMITBUTTON' : 'Enviar',
+          'CLEARBUTTON' : 'Limpiar',
+          'PLACEHOLDER_ANS' : 'Escribe tu respuesta aquí',
+          'END_MSG' : '¡Lo lograste! ',
+          'SCORE_MSG' : 'Obtuviste este resultado: '
+      }
+  }
+};
