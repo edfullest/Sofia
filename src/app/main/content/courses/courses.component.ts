ac6c4c11909780f42d55aee42d64a945685232eb
Ruben Hidalgo <get1595@gmail.com>
2018-03-13 14:41:54 -0600

    *Dashboard Feature* now courses can be searched by name

--- src/app/main/content/courses/courses.component.ts
+++ src/app/main/content/courses/courses.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FuseTranslationLoaderService } from '../../../core/services/translation-loader.service';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument, AngularFirestoreModule } from 'angularfire2/firestore';
-import { Router } from "@angular/router";
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { fuseAnimations } from '../../../core/animations';
 import { locale as english } from './i18n/en';
@@ -15,6 +15,7 @@ import { AuthService } from '../../../auth/auth.service';
 
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector   : 'courses-view',
   templateUrl: './courses.component.html',
   styleUrls  : ['./courses.component.scss'],
@@ -31,32 +32,36 @@ import { AuthService } from '../../../auth/auth.service';
     categoriesCollection: AngularFirestoreCollection<any[]>;
     categories: Observable<any[]>;
     categorySelected = 'All';
+
+    searchBarInput = '';
+
+
     
     constructor(public translationLoader: FuseTranslationLoaderService, 
                 public db: AngularFirestore, 
                 public router: Router,
-                public auth : AuthService)
+                public auth: AuthService)
     {
       this.auth.user.subscribe( userData => {
         this.coursesCollection = this.db.collection('courses', ref => 
-                               ref.where("createdBy", '==', userData.uid));
+                               ref.where('createdBy', '==', userData.uid));
         this.courses = this.coursesCollection.snapshotChanges().map(document => {
             return document.map(documentData => {
               const data = documentData.payload.doc.data();
               const id = documentData.payload.doc.id;
               // We check if the user already rated that course
-              var isRatedByUser = false
-              for (var uid in data.usersThatRated) {
-                if (uid == userData.uid){
-                  isRatedByUser = true
+              let isRatedByUser = false;
+              for (const uid in data.usersThatRated) {
+                if (uid === userData.uid){
+                  isRatedByUser = true;
                   break;
                 }                    
               }
-              return { id,isRatedByUser, ...data };
+              return { id, isRatedByUser, ...data };
             });
          });
        }
-      )
+      );
       this.translationLoader.loadTranslations(english, spanish);
       
 
@@ -65,20 +70,33 @@ import { AuthService } from '../../../auth/auth.service';
 
     }
 
-    redirectToGames(courseID){
-      this.router.navigate(["/teacher/courses/" +courseID + "/games"])
+    testInput(courseName: String){
+
+      const courseNameComparison = courseName.toLowerCase();
+      const searchBarInputComparison = this.searchBarInput.toLowerCase();
+
+      return courseNameComparison.indexOf(searchBarInputComparison) !== -1;
     }
+
+
+
+    redirectToGames(courseID){
+      this.router.navigate(['/teacher/courses/' + courseID + '/games']);
+    }
+
+
+
 
 
     deleteCourse(courseID: string){
       this.db.collection('courses').doc(courseID).snapshotChanges().subscribe(document => {
         const data = document.payload.data();
         this.auth.user.subscribe( userData => {
-          if (userData.uid == data.createdBy){
+          if (userData.uid === data.createdBy){
             this.db.collection('courses').doc(courseID).delete();
           }
-        })
-      })
+        });
+      });
 
       
     }
