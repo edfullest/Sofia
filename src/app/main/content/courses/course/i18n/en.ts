1f2bf40bee265b40b37fdf21c1b719c3b65bc14f
Andrea Osorio <a.wewe95@gmail.com>
2018-03-12 19:23:08 -0600

    Added Internationalization to Create Course

--- src/app/main/content/courses/course/i18n/en.ts
+++ src/app/main/content/courses/course/i18n/en.ts
@@ -7,10 +7,23 @@ export const locale = {
           'CREATEBUTTON' : 'Create Course',
           'CATEGORTY_ALL' : 'All courses',
           'TOOLTIP' : 'Edit / Delete',
-          'SELECT_CATEGORY' : 'Choice a category',
+          'SELECT_CATEGORY' : 'Select a category',
           'EDITBUTTON' : 'Edit',
-          'DELETEBUTTON' : 'Delete'
+          'DELETEBUTTON' : 'Delete',
+          'FILL_COURSE_INFO' : 'Fill out Your Course Information',
+          'COURSE_TITLE' : 'Course Title',
+          'COURSE_DESCRIPTION' : 'Course Description',
+          'COURSE_LENGTH' : 'Course Length',
+          'COURSE_DIFF' : 'Course Difficulty',
+          'COURSE_PRICE' : 'Course Price',
+          'TIME_UNIT' : 'Time Unit',
+          'BACK_BUTTON' : 'Back',
+          'SAVE_BUTTON' : 'Save',
+          'MONTHS' : 'Months',
+          'DAYS' : 'Days',
+          'EASY' : 'Easy',
+          'MEDIUM' : 'Medium',
+          'HARD' : 'Hard'
       }
-
   }
 };
--- src/app/main/content/courses/course/i18n/es.ts
+++ src/app/main/content/courses/course/i18n/es.ts
@@ -9,7 +9,21 @@ export const locale = {
           'TOOLTIP' : 'Editar/Eliminar',
           'SELECT_CATEGORY' : 'Selecciona una categoría',
           'EDITBUTTON' : 'Editar',
-          'DELETEBUTTON' : 'Eliminar'
+          'DELETEBUTTON' : 'Eliminar',
+          'FILL_COURSE_INFO' : 'Completa la información de tu curso',
+          'COURSE_TITLE' : 'Título del Curso',
+          'COURSE_DESCRIPTION' : 'Descripción del Curso',
+          'COURSE_LENGTH' : 'Longitud del Curso',
+          'COURSE_DIFF' : 'Dificultad del Curso',
+          'COURSE_PRICE' : 'Precio del Curso',
+          'TIME_UNIT' : 'Unidad de Tiempo',
+          'BACK_BUTTON' : 'Regresar',
+          'SAVE_BUTTON' : 'Guardar',
+          'MONTHS' : 'Meses',
+          'DAYS' : 'Días',
+          'EASY' : 'Fácil',
+          'MEDIUM' : 'Moderado',
+          'HARD' : 'Difícil'
       }
 
   }
