6a1658097203eb6ddbaf403e9c23a4cacfa8ad76
Ruben Hidalgo <get1595@gmail.com>
2018-04-08 20:29:03 -0500

    FAQ template added

--- src/app/main/content/landing-page/non-registered/FAQ/faq.component.ts
+++ src/app/main/content/landing-page/non-registered/FAQ/faq.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { ElementRef, Renderer, Input, Output, Optional, EventEmitter, ViewEncapsulation } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 import { Router } from '@angular/router';
 import { fuseAnimations } from '../../../../../core/animations';
 
 import { FuseTranslationLoaderService } from '../../../../../core/services/translation-loader.service';
+import { FuseUtils } from '../../../../../core/fuseUtils';
+
 import { locale as english } from './i18n/en';
 import { locale as spanish } from './i18n/es';
+
 
 
 @Component({
@@ -19,9 +23,32 @@ import { locale as spanish } from './i18n/es';
 
 export class FAQComponent{
 
+  faqs: any;
+    faqsFiltered: any;
+    step = 0;
+    searchInput;
+
   constructor(public translationLoader: FuseTranslationLoaderService)
   {
     this.translationLoader.loadTranslations(english, spanish);
+    this.searchInput = new FormControl('');
   }
+
+
+  setStep(index: number)
+    {
+        this.step = index;
+    }
+
+    nextStep()
+    {
+        this.step++;
+    }
+
+    prevStep()
+    {
+        this.step--;
+    }
+
 
 }
